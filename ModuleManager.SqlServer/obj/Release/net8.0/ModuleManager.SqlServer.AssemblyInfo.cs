6d90412f5b18a36d1742a495d43398ce82409ac9
Nguyễn Lam Trường <lamtruongnguyen7@gmail.com>
2024-06-18 15:43:28 +0700

    update

--- ModuleManager.SqlServer/obj/Release/net8.0/ModuleManager.SqlServer.AssemblyInfo.cs
+++ ModuleManager.SqlServer/obj/Release/net8.0/ModuleManager.SqlServer.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ModuleManager.SqlServer")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+519dc5afd20347c5bf4958a2c09186462aa8d406")]
 [assembly: System.Reflection.AssemblyProductAttribute("ModuleManager.SqlServer")]
